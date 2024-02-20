06059648b0722a72cb3b9bc961a6fbed5ffa84d7
Sawyer <sporte@ucsc.edu>
2024-02-19 17:26:11 -0800

    win con begin

---
+++ Hackathon_W2024/Assets/Scripts/WinCon.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class WinCon : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    public float speed;
+    Vector3 target;
+    [SerializeField] bool zoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        zoom = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("you win");
+            zoom = true;
+            target = transform.position;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (zoom)
+        {
+            StartCoroutine(cameraMove());
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, 3, speed);
+            //cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(target.x, target.y, cam.transform.position.z), .0125f);
+        }
+    }
+
+
+    IEnumerator cameraMove()
+    {
+        while (cam.transform.position != target)
+        {
+            cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(target.x, target.y, cam.transform.position.z), .125f);
+        }
+        
+        yield return new WaitForSeconds(2);
+    }
+}

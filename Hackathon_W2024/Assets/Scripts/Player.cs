3e5fc65709bbea4bb1f4e558f780d1eafb52a84d
Adam <57573881+AdamCarter11@users.noreply.github.com>
2024-02-19 19:20:43 -0800

    More work

--- Hackathon_W2024/Assets/Scripts/Player.cs
+++ Hackathon_W2024/Assets/Scripts/Player.cs
@@ -80,7 +80,7 @@ public class Player : MonoBehaviour
             if (closestObject == null)
             {
                 pickedUpObject.transform.parent = this.gameObject.transform;
-                pickedUpObject.transform.position = transform.position + Vector3.up * 1.5f;
+                pickedUpObject.transform.position = transform.position + Vector3.up * 1.5f; //transform.position + Vector3.up * 1.5f;
             }
             else
             {
